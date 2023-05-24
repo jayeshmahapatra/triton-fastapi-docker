8f4b3102d50a617fd916eda7a438d459ca9284ff
Jayesh <jayeshmahapatra@gmail.com>
2023-05-24 21:28:43 +0200

    added type hints and comments

--- fastapi/main.py
+++ fastapi/main.py
@@ -15,12 +15,26 @@ from utils import decode_img_to_numpy
 
 app = FastAPI()
 
-async def test_infer(triton_client,
-                    model_name,
-                    input_data,
-                    input_node, 
-                    output_node
-):
+async def test_infer(triton_client: grpcclient.InferenceServerClient, 
+                     model_name: str, 
+                     input_data: np.ndarray, 
+                     input_node: str, 
+                     output_node: str) -> grpcclient.InferResult:
+    
+    """
+    Perform inference using Triton server client.
+
+    Args:
+        triton_client (grpcclient.InferenceServerClient): Triton server client.
+        model_name (str): Name of the model to perform inference on.
+        input_data (np.ndarray): Input data for inference.
+        input_node (str): Input node name in the Triton model.
+        output_node (str): Output node name in the Triton model.
+
+    Returns:
+        grpcclient.InferResult: Inference results from Triton server.
+    """
+
     inputs = []
     outputs = []
 
@@ -36,21 +50,49 @@ async def test_infer(triton_client,
     return results
 
 
-
 @app.get("/")
 async def root():
+    """
+    Default root endpoint.
+
+    Returns:
+        dict: A simple greeting message.
+    """
     return {"message": "Hello World"}
+
 
 @app.get("/predict", response_model=ClassificationResult)
 async def predict(data: ClassificationRequest) -> ClassificationResult:
+    """
+    Perform prediction using the provided image data.
 
+    Args:
+        data (ClassificationRequest): Classification request containing image data.
+
+    Returns:
+        ClassificationResult: Classification result containing the predicted label.
+    """
+
+    # Decode base64 image into numpy array
     img = decode_img_to_numpy(data.base64_image, data.height, data.width)
-    triton_client = grpcclient.InferenceServerClient(url= "triton:8001")
-    triton_out = await test_infer(triton_client, model_name= "bee_vs_ant", input_data= img,
-                                input_node = "INPUT__0", output_node = "OUTPUT__0")
+
+    # Create a grpc InferenceServerClient to interface with the triton service
+    triton_client = grpcclient.InferenceServerClient(url="triton:8001")
+
+    # Do model inference and get results back
+    triton_out = await test_infer(
+        triton_client,
+        model_name="bee_vs_ant",
+        input_data=img,
+        input_node="INPUT__0",
+        output_node="OUTPUT__0"
+    )
+
+    # Cast model output as numpy
     logits = triton_out.as_numpy("OUTPUT__0")
     result = {}
     
+    # Based on logits, the image is predicted to be of a bee or an ant
     result['prediction'] = "bee" if logits[0].argmax() == 1 else "ant"
 
     return result
